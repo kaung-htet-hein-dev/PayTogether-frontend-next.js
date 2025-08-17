3a3a2a87dd06bdc7235836336effb988921a950b
Kaung-Htet-Hein <kaunghtethein.dev@gmail.com>
2025-08-17 21:24:49 +0800

    feat: next theme added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter, Poppins } from 'next/font/google';
 import './globals.css';
 import { ThemeProvider } from '@/providers';
+import ThemeSwitcher from '@/components/ThemeSwitcher';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -22,7 +23,10 @@ export default function RootLayout({
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body>
-        <ThemeProvider>{children}</ThemeProvider>
+        <ThemeProvider>
+          {children}
+          <ThemeSwitcher />
+        </ThemeProvider>
       </body>
     </html>
   );
