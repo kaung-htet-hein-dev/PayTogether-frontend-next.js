6decbf12cededfc138d489e10792618d0f93789f
Kaung <kaunghtethein.dev@gmail.com>
2025-08-19 16:01:14 +0800

    feat: styled typography example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,8 @@ export default function Home() {
       <div>
         <span className="inline-flex items-center justify-center rounded-md bg-indigo-500 p-2 shadow-lg"></span>
       </div>
-      <h3 className="text-gray-900 dark:text-white mt-5 text-base font-medium tracking-tight ">
-        Writes upside-down
-      </h3>
-      <p className="text-gray-500 dark:text-gray-400 mt-2 text-sm ">
+      <p className="">Writes upside-down</p>
+      <p className="text-gray-500 dark:text-gray-400 mt-2 text-sm">
         The Zero Gravity Pen can be used to write in any orientation, including upside-down. It even
         works in outer space.
       </p>
